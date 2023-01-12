d6d3e938775588abb6fb69205ec28ef7c97b9310
bayram <bayramk@gmail.com>
2023-01-12 11:43:13 +0300

    first push

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ build:
 	: run make install
 install:
 	mkdir -p $(DESTDIR)/usr/share/pardus/pardus-lightdm-greeter/module
+	mkdir -p $(DESTDIR)/usr/share/icons/hicolor/scalable/status/
 	cp -prfv module/* $(DESTDIR)/usr/share/pardus/pardus-lightdm-greeter/module/
+	cp -pf icon/*.svg $(DESTDIR)/usr/share/icons/hicolor/scalable/status/
+	#rm -rf $(DESTDIR)/usr/share/pardus/pardus-lightdm-greeter/data/icon
---
+++ module/_keyboard.py
@@ -0,0 +1,15 @@
+import asyncio
+
+def _klv_button_event(widget=None):
+    #qrpopover.popup()
+     os.system(get("screen-keyboard", "sh -c 'pkill e-keyboard;/usr/bin/e-keyboard login;'", "keyboard")+"&")
+     loginwindow.o("ui_entry_password").grab_focus()
+    #GLib.idle_add(qrkod_control_event)
+
+def module_init():
+    icon = Gtk.Image()
+    icon.set_from_file("/usr/share/icons/hicolor/scalable/status/keyboard.svg")
+    button = Gtk.Button(image=icon)
+    button.connect("clicked", _klv_button_event)
+    loginwindow.o("ui_box_bottom_right").pack_start(button, False, True, 10)
+    button.show_all()
