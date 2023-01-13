f02e50b4daeadce0a4e02589bf5ae9f325d7ca51
bayram <bayramk@gmail.com>
2023-01-13 09:42:10 +0300

    keyboard icon

--- module/_keyboard.py
+++ module/_keyboard.py
@@ -1,12 +1,19 @@
 import asyncio
-
+durum=0
 def _klv_button_event(widget=None):
-    #qrpopover.popup()
-     os.system(get("screen-keyboard", "sh -c 'pkill e-keyboard;/usr/bin/e-keyboard login;'", "keyboard")+"&")
+     #os.system(get("screen-keyboard", "sh -c 'pkill e-keyboard;/usr/bin/e-keyboard login;'", "keyboard")+"&")
+     #loginwindow.o("ui_entry_password").grab_focus()
+     global durum
+     if durum==0:
+         durum=1
+         os.system(get("screen-keyboard", "sh -c 'pkill e-keyboard;/usr/bin/e-keyboard login;'", "keyboard")+"&")
+     elif durum==1:
+         durum=0
+         os.system(get("screen-keyboard", "sh -c 'pkill e-keyboard;'", "keyboard")+"&")
      loginwindow.o("ui_entry_password").grab_focus()
-    #GLib.idle_add(qrkod_control_event)
 
 def module_init():
+    global durum
     icon = Gtk.Image()
     icon.set_from_file("/usr/share/icons/hicolor/scalable/status/keyboard.svg")
     button = Gtk.Button(image=icon)
@@ -14,3 +21,5 @@ def module_init():
     button.connect("clicked", _klv_button_event)
     loginwindow.o("ui_box_bottom_right").pack_start(button, False, True, 10)
     button.show_all()
+    #loginwindow.o("ui_entry_passwords").connect("focus-in-event",_klv_button_event)
+
